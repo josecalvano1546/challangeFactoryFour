8c20c43125ee9a2ac0a7e3defc27552b789ad962
“jcalvano” <102161895+jcalvano@users.noreply.github.com>
2024-09-10 00:48:10 -0300

    update project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";  // Usa el nuevo import de 'react-dom/client'
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));  // Crea el root de React
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
 reportWebVitals();
